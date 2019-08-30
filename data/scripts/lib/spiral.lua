2144360c0cde90975328c27a2ee874ee526b4d28
VincentCross <vincent.cross.main@gmail.com>
2019-08-30 23:39:33 +1000

    Initial proof of concept of spiraling through grid coordinates.

---
+++ data/scripts/lib/spiral.lua
@@ -0,0 +1,43 @@
+package.path = package.path .. ";data/scripts/lib/?.lua"
+
+-- namespace Spiral
+Spiral = {}
+
+include("callable")
+
+function Spiral.initialize()
+	local player = Player()
+    player:registerCallback("onShowGalaxyMap", "handlePrintSpiral")
+	eprint("Registered Callback onShowGalaxyMap")
+end
+
+
+function Spiral.handlePrintSpiral()
+	eprint("Running handlePrintSpiral")
+	Spiral.printSpiral(10,10)
+end
+
+function Spiral.printSpiral(endingRow, endingColumn)
+	local x = 0
+	local y = 0
+	local d = 1
+	local m = 1
+	
+	repeat
+		while 2 * x * d < m do
+			eprint("%i - %i", x, y)
+			x = x + d
+		end
+		
+		while 2 * y * d < m do
+			eprint("%i - %i", x, y)
+			y = y + d
+		end
+		
+		d = -1 * d
+		m = m + 1
+	until y == endingRow or x == endingColumn
+	
+	--print last square
+	eprint("%i - %i", x, y)
+end
---
+++ data/scripts/player/init.lua
@@ -0,0 +1,3 @@
+if onServer() then
+    Player():addScriptOnce("data/scripts/lib/spiral.lua")
+end
